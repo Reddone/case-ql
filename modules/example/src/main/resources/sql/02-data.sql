8863c132111e740d5530be017998c8502a717dc2
Reddone <simone88.rm2@gmail.com>
2020-05-03 17:38:04 +0200

    fixed env

--- modules/example/src/main/resources/sql/02-data.sql
+++ modules/example/src/main/resources/sql/02-data.sql
@@ -1,15 +1,15 @@
-INSERT INTO public.developer (id, name, age, team_leader_id) VALUES
-(),
-();
+INSERT INTO public.developer (name, age, team_leader_id) VALUES
+('name1', 1, NULL),
+('name2', 2, NULL);
 
-INSERT INTO public.project (id, title, description) VALUES
-(),
-();
+INSERT INTO public.project (title, description) VALUES
+('title1', NULL),
+('title2', NULL);
 
 INSERT INTO public.developer_project_link (developer_id, project_id) VALUES
-(),
-();
+(1, 1),
+(2, 2);
 
-INSERT INTO public.task (id, label, description, deadline, project_id) VALUES
-(),
-();
+INSERT INTO public.task (label, description, deadline, project_id) VALUES
+('label1', NULL, '2014-07-02 06:14:00', 1),
+('label2', NULL, '2014-07-02 06:14:00', 2);
