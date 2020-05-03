4bfe3e48087feba42ef7f8ab5640e869464e1269
Reddone <simone88.rm2@gmail.com>
2020-05-03 20:37:01 +0200

    trying docker

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,10 @@ RUN unzip case-ql-example-0.0.1.zip && mv case-ql-example-0.0.1 stage
 
 EXPOSE 4000
 
+ENV DOOBIE_NUM_THREADS = 10
+ENV DOOBIE_URL = "jdbc:postgresql://db:5432/pgdb"
+ENV DOOBIE_USER = "pguser"
+ENV DOOBIE_PASSWORD = "pguser"
+
 ENTRYPOINT ["/bin/bash", "stage/bin/case-ql-example"]
 CMD []
--- modules/example/src/main/resources/sql/02-data.sql
+++ modules/example/src/main/resources/sql/02-data.sql
@@ -1,15 +1,34 @@
 INSERT INTO public.developer (name, age, team_leader_id) VALUES
-('name1', 1, NULL),
-('name2', 2, NULL);
+('name1', 32, NULL),
+('name2', 1, 1),
+('name3', 1, 1),
+('name4', 23, NULL),
+('name5', 2, 4),
+('name6', 2, 4),
+('name7', 42, NULL),
+('name8', 3, 7);
 
 INSERT INTO public.project (title, description) VALUES
-('title1', NULL),
-('title2', NULL);
+('title1', 'description1'),
+('title2', NULL),
+('title3', 'description3');
 
 INSERT INTO public.developer_project_link (developer_id, project_id) VALUES
 (1, 1),
-(2, 2);
+(2, 1),
+(3, 1),
+(5, 1),
+(4, 2),
+(5, 2),
+(6, 2),
+(3, 2),
+(7, 3),
+(8, 3);
 
 INSERT INTO public.task (label, description, deadline, project_id) VALUES
-('label1', NULL, '2014-07-02 06:14:00', 1),
-('label2', NULL, '2014-07-02 06:14:00', 2);
+('label1', 'description1', '2020-01-01 01:01:01', 1),
+('label2', 'description2', '2020-02-02 02:02:02', 1),
+('label3', NULL, '2020-01-01 01:01:01', 2),
+('label4', NULL, '2020-02-02 02:02:02', 2),
+('label5', 'description5', '2020-03-03 03:03:03', 3),
+('label6', NULL, '2020-03-03 03:03:03', 3);
